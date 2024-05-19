75bcfbe382c8c6e59f6c1e6d4c930a0bff549143
udaynarwal72 <udaynarwal72@gmail.com>
2024-05-19 21:38:39 +0530

    update

--- model/userModel.js
+++ model/userModel.js
@@ -5,8 +5,9 @@ const userSchema = new mongoose.Schema({
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     // Use a Map to handle dynamic custom properties
+    city: { type: String, required: true },
     pushemail: { type: Boolean, default: true },
 });
 //very very important if a user got a unique email then only we will push that
-const UserDetail = mongoose.model('myUserDetail', userSchema);
+const UserDetail = mongoose.model('asmdyUserDetails', userSchema);
 module.exports = UserDetail;
