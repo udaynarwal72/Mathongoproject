079952bea6260722cde5ad4c60159fd093567092
udaynarwal72 <udaynarwal72@gmail.com>
2024-05-19 22:37:29 +0530

    update

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -25,7 +25,7 @@ const storage = multer.diskStorage({
     }
 });
 
-const upload = multer({ storage: storage });
+const upload = multer({ dest: 'public/uploads/' });
 //1st task
 router.post('/', (req, res) => {
     res.send('Hello World');
